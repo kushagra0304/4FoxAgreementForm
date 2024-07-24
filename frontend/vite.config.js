d78cfccf24d4adaab5048fea8fe3e39a37569fa0
NEWHELL69 <kushagra0304@gmail.com>
2024-07-25 01:43:18 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../backend/public', 
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
-    build: {
-      outDir: '../backend/public', 
-      emptyOutDir: true,
-    },
     proxy: {
       '/api': {
         target: 'http://localhost:10000',
